934a553743e4ff07f90816459aea7161e6048b63
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-11 13:08:06 -0700

    further test stability of keys. add test.js with future solution for reference.
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>
    Co-authored-by: Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,6 @@ function convert(numInput) {
   if (numInput < 1 || numInput > 3999) {
     return alert("Please enter a number between 1 and 3,999");
   } else if (numInput === 1 || numInput === 5 || numInput === 10 || numInput === 50 || numInput === 100 || numInput === 500 || numInput === 1000) {
-//    newString = (numInput).toString("");
-    // let keys = Object.keys(numerals).numInput;
-    // console.log(keys);
-//    console.log( item );
     const key = Object.keys(numerals)[Object.values(numerals).indexOf(numInput)];
     console.log(key);
   } else {
